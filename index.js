8deb9d58e6ead24b99c9741fcf86d753060156df
Haidar Ahmad Faiq <haidarahmadfaiq@gmail.com>
2023-11-18 19:43:48 +0700

    add file index.js

---
+++ index.js
@@ -0,0 +1,32 @@
+/*
+ * MIT License
+ * Copyright (c) 2023 Haidar Ahmad Faiq
+ * --- datatype-validation --- *
+*/
+
+const string = require('./lib/string.js');
+const number = require('./lib/number.js');
+const boolean = require('./lib/boolean.js');
+const datetime = require('./lib/datetime.js');
+
+module.exports = {
+  // String
+  isString: string.isString,
+  toString: string.toString,
+
+  // Number
+  isInt: number.isInt,
+  isFloat: number.isFloat,
+  toInt: number.toInt,
+  toFloat: number.toFloat,
+
+  // Boolean
+  isBoolean: boolean.isBoolean,
+  isBoolTrue: boolean.isBoolTrue,
+  isBoolFalse: boolean.isBoolFalse,
+
+  // Datetime
+  isDate: datetime.isDate,
+  isTime: datetime.isTime,
+  isDateTime: datetime.isDateTime,
+};
