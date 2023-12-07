c5fd39356733a1a17a844d9cf5135072aa854504
Haidar Ahmad Faiq <haidarahmadfaiq@gmail.com>
2023-12-08 04:17:21 +0700

    added error message

--- datatype.js
+++ datatype.js
@@ -2,9 +2,15 @@ const datatype = (function() {
   // Modul string
   const string = {
     isString: (value) => {
+      if(value === undefined) {
+        throw Error('Enter the value of the function isString');
+      }
       return typeof value === 'string' && value.trim() !== '';
     },
     toString: (value) => {
+      if(value === undefined) {
+        throw Error('Enter the value of the function toString');
+      }
       return String(value);
     }
   };
@@ -12,15 +18,27 @@ const datatype = (function() {
   // Modul number
   const number = {
     isInt: (value) => {
+      if(value === undefined) {
+        throw Error('Enter the value of the function isInt');
+      }
       return typeof value === 'number' && Number.isInteger(value);
     },
     isFloat: (value) => {
+      if(value === undefined) {
+        throw Error('Enter the value of the function isFloat');
+      }
       return typeof value === 'number' && !Number.isInteger(value);
     },
     toInt: (value) =>  {
+      if(value === undefined) {
+        throw Error('Enter the value of the function toInt');
+      }
       return parseInt(value, 10);
     },
     toFloat: (value) => {
+      if(value === undefined) {
+        throw Error('Enter the value of the function toFloat');
+      }
       return parseFloat(value).toFixed(1);
     }
   };
@@ -28,12 +46,21 @@ const datatype = (function() {
   // Modul boolean
   const boolean = {
     isBoolean: (value) => {
+      if(value === undefined) {
+        throw Error('Enter the value of the function isBoolean');
+      }
       return typeof value === 'boolean';
     },
     isBoolTrue: (value) => {
+      if(value === undefined) {
+        throw Error('Enter the value of the function isBoolTrue');
+      }
       return isBoolean(value) && value === true;
     },
     isBoolFalse: (value) => {
+      if(value === undefined) {
+        throw Error('Enter the value of the function isBoolFalse');
+      }
       return isBoolean(value) && value === false;
     }
   };
@@ -41,14 +68,23 @@ const datatype = (function() {
   // Modul datetime
   const datetime = {
     isDate: (value) => {
+      if(value === undefined) {
+        throw Error('Enter the value of the function isDate');
+      }
       const dateFormatRegex = /^\d{2}\D\d{2}\D\d{4}$/;
       return dateFormatRegex.test(value);
     },
     isTime: (value) => {
+      if(value === undefined) {
+        throw Error('Enter the value of the function isTime');
+      }
       const timeFormatRegex = /^\d{2}\D\d{2}\D\d{2}$/;
       return timeFormatRegex.test(value);
     },
     isDateTime: (value) => {
+      if(value === undefined) {
+        throw Error('Enter the value of the function isDateTime');
+      }
       const dateTimeFormatRegex = /^\d{2}\D\d{2}\D\d{4} \d{2}\D\d{2}\D\d{2}$/;
       return dateTimeFormatRegex.test(value);
     }
